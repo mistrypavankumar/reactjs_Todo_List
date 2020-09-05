349f6be65f866aeaf4ebe970496b8bd83482f37e
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-09-06 03:09:24 +0530

    Updates

---
+++ precache-manifest.606b5a8f9b652a9d98d828a56b73f39c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4bb6012cc36985a926f23219c894a54",
+    "url": "/reactjs_Todo_List/index.html"
+  },
+  {
+    "revision": "4c9110640854bddc0a06",
+    "url": "/reactjs_Todo_List/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a522cdfb672504e922e4",
+    "url": "/reactjs_Todo_List/static/css/main.56cc3ac7.chunk.css"
+  },
+  {
+    "revision": "4c9110640854bddc0a06",
+    "url": "/reactjs_Todo_List/static/js/2.4fea5f40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs_Todo_List/static/js/2.4fea5f40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a522cdfb672504e922e4",
+    "url": "/reactjs_Todo_List/static/js/main.5bbc46e3.chunk.js"
+  },
+  {
+    "revision": "76cbf99903212d40caa7",
+    "url": "/reactjs_Todo_List/static/js/runtime-main.4ee234b1.js"
+  }
+]);
